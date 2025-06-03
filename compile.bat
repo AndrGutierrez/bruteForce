d3d673a28bbeb9081fba95b37c7292a93800d819
Andres Gutierrrez <andresgutierrezdev@gmail.com>
2025-06-02 23:55:42 -0400

    :hammer: build(windows): compile bat

--- compile.bat
+++ compile.bat
@@ -1,7 +1,2 @@
 @echo off
-set OPENSSL_PATH="C:\OpenSSL-Win64"  REM Change this to your OpenSSL install path
-
-gcc main.c brute_force.c semaforo.c -o decrypt.exe ^
-    -I%OPENSSL_PATH%\include ^
-    -L%OPENSSL_PATH%\lib ^
-    -lssl -lcrypto -lws2_32 -lgdi32
+gcc main.c brute_force.c semaforo.c -o decrypt.exe -lssl -lcrypto
