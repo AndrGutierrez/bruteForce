6f1d9a813abf09959a87efd2c717c40c459ea4c7
Andres Gutierrrez <andresgutierrezdev@gmail.com>
2025-06-04 16:50:21 -0400

    asldfk

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include "brute_force.h"
 #include "semaforo.h"
 #include <openssl/evp.h>
 #include <stdbool.h>
@@ -12,22 +11,16 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  // Using time() (seconds precision)
   time_t start_time, end_time;
   time(&start_time);
 
-  // Or using gettimeofday() (microseconds precision)
   struct timeval start, end;
   gettimeofday(&start, NULL);
 
   semaforo(argv);
 
-  // For time()
   time(&end_time);
-  double time_taken = difftime(end_time, start_time);
-  printf("Brute force took %.2f seconds (wall-clock time)\n", time_taken);
 
-  // For gettimeofday()
   gettimeofday(&end, NULL);
   double time_taken_us =
       (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1e6;
