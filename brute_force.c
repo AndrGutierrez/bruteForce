fefa96e3c052e8e2b3208e107e46bbe9be3c06f7
Andres Gutierrrez <andresgutierrezdev@gmail.com>
2025-06-04 20:00:59 -0400

    removing debugging comments

--- brute_force.c
+++ brute_force.c
@@ -3,12 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_LENGTH 4 // Maximum password length to try
+#define MAX_LENGTH 4
 #include "brute_force.h"
 
-// int get_number_of_guesses(){
-//   for()
-// }
 bool brute_force_md5(const char *target_hash, int length, int *guesses,
                      int batch_size, int start[4]) {
   const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -38,25 +35,15 @@ bool brute_force_md5(const char *target_hash, int length, int *guesses,
   unsigned char digest[EVP_MAX_MD_SIZE];
   unsigned int digest_len;
 
-  // cada hilo buscará 1242497
   int guess_number[4] = {0, 0, 0, 0};
   while (!found) {
 
     for (int i = 0; i < length; i++) {
       guess[i] = charset[indices[i]];
       guess_number[i] = indices[i];
-      // TODO: cada hilo buscará 1242497
-      // aca están como los numeros de letra a los que accede, usa esto para
-      // dividir el trabajo
-      // printf("#### %s\n", charset);
     }
     guess[length] = '\0';
-    // printf("#### %d %d %d %d\n", guess_number[0], guess_number[1],
-    //        guess_number[2], guess_number[3]);
     *guesses += 1;
-    // printf("%d \n", guesses);
-
-    // printf("### %s\n", guess);
     EVP_DigestInit_ex(ctx, md, NULL);
     EVP_DigestUpdate(ctx, guess, strlen(guess));
     EVP_DigestFinal_ex(ctx, digest, &digest_len);
@@ -81,26 +68,14 @@ bool brute_force_md5(const char *target_hash, int length, int *guesses,
       pos++;
     }
 
-    // printf("#### %d\n", pos);
     if (pos == length)
       break;
 
-    // printf("%d \n", *guesses);
-    // printf("%d, %d, %d\n", *guesses % batch_size, *guesses, batch_size);
     if ((*guesses % 1242497) == 0) {
-      // printf("### %d\n", *guesses);
-      // printf("### %s\n", guess);
-      // // printf("%d, %d\n", *guesses, start);
       printf("#### %d %d %d %d\n", guess_number[0], guess_number[1],
              guess_number[2], guess_number[3]);
-      // // }
       break;
     }
-    // if (*guesses == 3727490) {
-    //
-    // printf("#### %d %d %d %d\n", guess_number[0], guess_number[1],
-    //        guess_number[2], guess_number[3]);
-    // }
   }
 
   EVP_MD_CTX_free(ctx);
@@ -112,29 +87,23 @@ bool brute_force_md5(const char *target_hash, int length, int *guesses,
 void *brute_force(void *param) {
   ThreadArgs *args = (ThreadArgs *)param;
   char **argv = args->argv;
-  int batch_size = args->batch_size; // Unused (just for testing)
+  int batch_size = args->batch_size;
   int start[4];
   for (int i = 0; i < 4; i++) {
     start[i] = args->start[i];
   }
 
   const char *target_hash = argv[1];
-  // printf("Attempting to brute force MD5 hash: %s\n", target_hash);
-  // printf("Breakpoint received (unused): %d\n", breakpoint); // Debug print
 
   int guesses = 0;
   int init = 1;
   if (start[0] == 0 && start[1] == 0 && start[2] == 0 && start[3] == 0)
     init = 1;
   else {
-
-    // printf("### añañin %d\n");
     init = 4;
   }
   for (int length = init; length <= MAX_LENGTH; length++) {
-    // printf("Trying passwords of length %d...\n", length);
     if (brute_force_md5(target_hash, length, &guesses, batch_size, start)) {
-      // printf("### guesses: %d \n", guesses);
       pthread_exit(0); // Exit thread on success
     }
   }
--- semaforo.c
+++ semaforo.c
@@ -2,11 +2,10 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-// Global variables shared by threads
 int sum = 0, numero;
 pthread_mutex_t mutex_acceso;
 
-const int NUM_THREADS = 6; // Now using 5 threads
+const int NUM_THREADS = 6;
 const int BATCH_SIZE = 12424967;
 const int BREAKPOINTS[6][4] = {{0, 0, 0, 0},     {8, 25, 42, 7},
                                {17, 51, 33, 16}, {35, 51, 16, 34},
@@ -19,13 +18,10 @@ int semaforo(char *argv[]) {
   pthread_attr_t attr;
   ThreadArgs thread_args[NUM_THREADS];
 
-  // Initialize mutex
   pthread_mutex_init(&mutex_acceso, NULL);
 
-  // Initialize thread attributes
   pthread_attr_init(&attr);
 
-  // Create threads
   for (int i = 0; i < NUM_THREADS; i++) {
     thread_args[i].argv = argv;
     thread_args[i].batch_size = BATCH_SIZE;
@@ -35,17 +31,14 @@ int semaforo(char *argv[]) {
 
     if (pthread_create(&threads[i], &attr, brute_force,
                        (void *)&thread_args[i])) {
-      // perror("Failed to create thread");
       exit(EXIT_FAILURE);
     }
   }
 
-  // Wait for all threads to complete
   for (int i = 0; i < NUM_THREADS; i++) {
     pthread_join(threads[i], NULL);
   }
 
-  // Clean up
   pthread_mutex_destroy(&mutex_acceso);
 
   return 0;
